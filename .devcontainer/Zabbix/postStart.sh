b8a1195590ca369558242e3717837e637fa4769d
Mick Morrow <mmorrow24work@gmail.com>
2026-03-11 18:33:29 +0000

    Update postStart.sh

--- .devcontainer/Zabbix/postStart.sh
+++ .devcontainer/Zabbix/postStart.sh
@@ -9,10 +9,7 @@ echo "Zabbix Login User=Admin / Password=zabbix" >> zabbix.txt
 # Start Zabbix stack
 echo "*** Start Zabbix stack ***********************************************************************************************************"
 cd git/zabbix-docker
-docker compose \
-  -f docker-compose_v3_alpine_mysql_snmptraps_latest.yaml \
-  -f docker-compose.override.yml \
-  up -d
+docker compose -f .devcontainer/Zabbix/docker-compose_v3_alpine_mysql_snmptraps_latest.yaml up -d
 echo "*** Complete Zabbix stack ***********************************************************************************************************"
 
 # URLs (Codespaces auto-forwards)
