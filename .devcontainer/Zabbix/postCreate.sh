698252acf33c0bf0f19255c37d560e88fc34320c
Mick Morrow <mmorrow24work@gmail.com>
2026-03-11 18:17:22 +0000

    Create postCreate.sh

---
+++ .devcontainer/Zabbix/postCreate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo apt-get update
+sudo apt-get install -y curl jq git htop btop eza
+
+# Clone zabbix repos
+mkdir -p git
+cd git
+test -d zabbix-docker || git clone https://github.com/zabbix/zabbix-docker.git
