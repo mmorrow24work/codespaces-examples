4b45d9b9dda51b0eefae3999a8bbe8b172d3d516
Mick Morrow <mmorrow24work@gmail.com>
2026-03-11 19:18:31 +0000

    Update postStart.sh

--- .devcontainer/Zabbix/postStart.sh
+++ .devcontainer/Zabbix/postStart.sh
@@ -8,9 +8,10 @@ echo "Zabbix Login User=Admin / Password=zabbix" >> zabbix.txt
 
 # Start Zabbix stack
 echo "*** Start Zabbix stack ***********************************************************************************************************"
-cp .devcontainer/Zabbix/docker-compose_v3_alpine_mysql_snmptraps_latest.yaml git/zabbix-docker/.
+docker network create --driver bridge digital-twin
+cp .devcontainer/Zabbix/* git/zabbix-docker/.
 cd git/zabbix-docker
-docker compose -f ./docker-compose_v3_alpine_mysql_snmptraps_latest.yaml up -d
+docker compose -f ./docker-compose_v3_alpine_mysql_snmptraps_latest.yaml -f ./docker-compose.override.yml up -f -d
 echo "*** Complete Zabbix stack ***********************************************************************************************************"
 
 # URLs (Codespaces auto-forwards)
