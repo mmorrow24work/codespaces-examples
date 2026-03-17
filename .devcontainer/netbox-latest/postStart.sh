7395e22a1524d43ca5910a15c91eda20f48fac11
Mick Morrow <mmorrow24work@gmail.com>
2026-03-17 12:52:38 +0000

    Update postStart.sh

--- .devcontainer/netbox-latest/postStart.sh
+++ .devcontainer/netbox-latest/postStart.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 echo "logs:/workspaces/.codespaces/.persistedshare" > netbox-only.txt
-echo "User=netbox / Password=admin" >> netbox-only.txt
+echo "User=netbox / Password=pdEpsCzU9K8D!" >> netbox-only.txt
 
 # URLs (Codespaces auto-forwards)
 echo "Netbox UI: http://localhost:8080"
-echo "Netbox Login User=admin / Password=admin"
+echo "Netbox Login User=netbox / Password=pdEpsCzU9K8D!"
